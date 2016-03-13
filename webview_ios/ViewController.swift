0b20b4c2a64ecde9a872cd24dbc1b11484a24c6d
ciroceissler <ciro.ceissler@gmail.com>
2016-03-13 01:42:51 -0300

    initial version. web version iOS test.

--- webview_ios/ViewController.swift
+++ webview_ios/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.google.com.br/")
+        let request = NSURLRequest(URL: url!)
+        
+        webView.loadRequest(request)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
